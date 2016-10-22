5ca756265d935560a5fabb922623aa698ab7fbeb
Raymond Luong <raymond.n.luong@gmail.com>
2016-10-21 18:04:17 -1000

    Implement getter and setter methods

--- vault.js
+++ vault.js
@@ -1,4 +1,21 @@
 'use strict';
 module.exports = function() {
-  
+
+  let vault = {};
+
+  function _setValue(key, value) {
+    vault.key = value;
+  };
+
+  function _getValue(key) {
+    if (!vault.key) {
+      return null
+    }
+    return vault.key
+  };
+
+  return {
+    setValue: _setValue,
+    getValue: _getValue
+  }
 };
